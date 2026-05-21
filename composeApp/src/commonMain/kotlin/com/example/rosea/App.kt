b58e0f79298735a54c4693f69dc65d5162aea886
04-123140067-AndiniRahmaKemala <andini.123140067@student.itera.ac.id>
2026-05-21 23:52:34 +0700

    Sprint 3

--- composeApp/src/commonMain/kotlin/com/example/rosea/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/rosea/App.kt
@@ -1,16 +1,16 @@
 package com.example.rosea
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
-import com.example.rosea.presentation.navigation.AppNavHost
+import com.example.rosea.presentation.screens.main.MainScreen // 👈 Import rumah baru kita
+import com.example.rosea.presentation.theme.RoseaTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        val navController = rememberNavController()
-        AppNavHost(navController = navController)
+    RoseaTheme {
+        // Kita tidak lagi memanggil AppNavHost langsung di sini.
+        // Semuanya sekarang dibungkus dan diatur oleh MainScreen.
+        MainScreen()
     }
 }
