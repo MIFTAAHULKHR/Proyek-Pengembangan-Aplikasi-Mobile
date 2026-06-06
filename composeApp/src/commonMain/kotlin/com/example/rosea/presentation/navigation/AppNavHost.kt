62d2d24ccff754a97a0c348bd9ad2ae7024e9c71
04-123140067-AndiniRahmaKemala <andini.123140067@student.itera.ac.id>
2026-06-07 03:20:35 +0700

    fix: Resolve navigation errors and sync AppNavHost with Routes

--- composeApp/src/commonMain/kotlin/com/example/rosea/presentation/navigation/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/com/example/rosea/presentation/navigation/AppNavHost.kt
@@ -1,17 +1,7 @@
 package com.example.rosea.presentation.navigation
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -22,13 +12,6 @@ import com.example.rosea.presentation.screens.cart.CartScreen
 import com.example.rosea.presentation.screens.detail.DetailScreen
 import com.example.rosea.presentation.screens.home.HomeScreen
 import com.example.rosea.presentation.screens.profile.ProfileScreen
-import com.example.rosea.presentation.screens.profile.edit.EditProfileScreen
-import com.example.rosea.presentation.screens.profile.notifications.NotificationScreen
-import com.example.rosea.presentation.screens.profile.vouchers.VoucherScreen
-import com.example.rosea.presentation.screens.profile.history.OrderHistoryScreen
-import com.example.rosea.presentation.screens.profile.settings.SettingsScreen
-import com.example.rosea.presentation.screens.profile.address.AddressManagementScreen
-import com.example.rosea.presentation.screens.profile.help.HelpSupportScreen
 
 @Composable
 fun AppNavHost(
@@ -80,42 +63,14 @@ fun AppNavHost(
             ProfileScreen(
                 onNavigateBack = { navController.popBackStack() },
                 onNavigateToCart = { navController.navigate(Routes.CART) },
-                onNavigateToEditProfile = { navController.navigate(Routes.EDIT_PROFILE) },
-                onNavigateToNotifications = { navController.navigate(Routes.NOTIFICATIONS) },
-                onNavigateToVouchers = { navController.navigate(Routes.VOUCHERS) },
-                onNavigateToHistory = { navController.navigate(Routes.HISTORY) },
-                onNavigateToAddress = { navController.navigate(Routes.ADDRESS_MANAGEMENT) },
-                onNavigateToHelp = { navController.navigate(Routes.HELP_SUPPORT) },
-                onNavigateToSettings = { navController.navigate(Routes.SETTINGS) }
+                onNavigateToEditProfile = { /* TODO */ },
+                onNavigateToNotifications = { /* TODO */ },
+                onNavigateToVouchers = { /* TODO */ },
+                onNavigateToHistory = { /* TODO */ },
+                onNavigateToAddress = { /* TODO */ },
+                onNavigateToHelp = { /* TODO */ },
+                onNavigateToSettings = { /* TODO */ }
             )
-        }
-
-        composable(Routes.EDIT_PROFILE) {
-            EditProfileScreen(onNavigateBack = { navController.popBackStack() })
-        }
-
-        composable(Routes.NOTIFICATIONS) {
-            NotificationScreen(onNavigateBack = { navController.popBackStack() })
-        }
-
-        composable(Routes.VOUCHERS) {
-            VoucherScreen(onNavigateBack = { navController.popBackStack() })
-        }
-
-        composable(Routes.HISTORY) {
-            OrderHistoryScreen(onNavigateBack = { navController.popBackStack() })
-        }
-
-        composable(Routes.SETTINGS) {
-            SettingsScreen(onNavigateBack = { navController.popBackStack() })
-        }
-
-        composable(Routes.ADDRESS_MANAGEMENT) {
-            AddressManagementScreen(onNavigateBack = { navController.popBackStack() })
-        }
-
-        composable(Routes.HELP_SUPPORT) {
-            HelpSupportScreen(onNavigateBack = { navController.popBackStack() })
         }
     }
 }
